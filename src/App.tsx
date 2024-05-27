2af94ba56875afb96b972b8f9152dbe461514d22
Prabhjot Sidhu <psidhu@pi.local>
2024-05-27 13:24:40 -0400

    building around the xzdarcy timeline

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,187 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Timeline, TimelineState, TimelineAction, TimelineEffect, TimelineRow} from '@xzdarcy/react-timeline-editor';
+import { Switch } from 'antd';
+import { cloneDeep } from 'lodash';
+import React, { useRef, useState } from 'react';
+import { CustomRender0, CustomRender1 } from './custom';
+import './index.less';
+import TimelinePlayer from './player';
+import audioControl from './audioControl';
+import lottieControl from './lottieControl';
 
-function App() {
+
+/////////////////////////////////////////////////////////////////////////////data control
+
+const scaleWidth = 160;
+const scale = 5;
+const startLeft = 20;
+
+interface CustomTimelineAction extends TimelineAction {
+  data: {
+    src: string;
+    name: string;
+    subtitleNumber: number;
+    metaData: string;
+  };
+}
+
+interface CusTomTimelineRow extends TimelineRow {
+  actions: CustomTimelineAction[];
+}
+
+const mockEffect: Record<string, TimelineEffect> = {
+  effect0: {
+    id: 'effect0',
+    name: 'effect0',
+    source: {
+      start: ({ action, engine, isPlaying, time }) => {
+        if (isPlaying) {
+          const src = (action as CustomTimelineAction).data.src;
+          audioControl.start({ id: src, src, startTime: action.start, engine, time });
+          console.log("data: ", (action as CustomTimelineAction));
+        }
+      },
+      enter: ({ action, engine, isPlaying, time }) => {
+        if (isPlaying) {
+          const src = (action as CustomTimelineAction).data.src;
+          audioControl.start({ id: src, src, startTime: action.start, engine, time });
+        }
+      },
+      leave: ({ action, engine }) => {
+        const src = (action as CustomTimelineAction).data.src;
+        audioControl.stop({ id: src, engine });
+      },
+      stop: ({ action, engine }) => {
+        const src = (action as CustomTimelineAction).data.src;
+        audioControl.stop({ id: src, engine });
+      },
+    },
+  },
+  effect1: {
+    id: 'effect1',
+    name: 'effect1',
+    source: {
+      enter: ({ action, time }) => {
+        const src = (action as CustomTimelineAction).data.src;
+        lottieControl.enter({ id: src, src, startTime: action.start, endTime: action.end, time });
+        console.log("data: ", (action as CustomTimelineAction));
+      },
+      update: ({ action, time }) => {
+        const src = (action as CustomTimelineAction).data.src;
+        lottieControl.update({ id: src, src, startTime: action.start, endTime: action.end, time });
+      },
+      leave: ({ action, time }) => {
+        const src = (action as CustomTimelineAction).data.src;
+        lottieControl.leave({ id: src, startTime: action.start, endTime: action.end, time });
+      },
+    },
+  },
+};
+
+const mockData: CusTomTimelineRow[] = [
+  {
+    id: '0',
+    actions: [
+      {
+        id: 'action3',
+        start: 0,
+        end: 20,
+        effectId: 'effect0',
+        data: {
+          src: '/lottie/lottie1/data.json',
+          name: 'Hello',
+          subtitleNumber: 1,
+          metaData: null,
+        },
+      },
+      {
+        id: 'action4',
+        start: 22,
+        end: 24,
+        effectId: 'effect1',
+        data: {
+          src: '/lottie/lottie1/data.json',
+          name: 'World',
+          subtitleNumber: 2,
+          metaData: null,
+        },
+      },
+    ],
+  },
+];  
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+const defaultEditorData = cloneDeep(mockData);
+
+const App = () => {
+
+  const [data, setData] = useState(defaultEditorData);
+  const timelineState = useRef<TimelineState>();
+  const playerPanel = useRef<HTMLDivElement>(null);
+  const autoScrollWhenPlay = useRef<boolean>(true);
+
+  const addSubtitle = () => {
+    const subtitleObject = {
+      id: 'action5',
+      start: 27,
+      end: 31,
+      effectId: 'effect1',
+      data: {
+        src: '/lottie/lottie1/data.json',
+        name: 'My Name is Prabhjot',
+        subtitleNumber: 3,
+        metaData: null,
+      }
+    };
+    const tempData = cloneDeep(defaultEditorData);
+    tempData[0].actions.push(subtitleObject);
+    setData(tempData);
+  }
+
+  console.log(timelineState);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="timeline-editor-engine">
+      <div>
+        <button onClick={() => addSubtitle()}>
+          Click To Add Subtitle: "New Subtitle"
+        </button>
+      </div>
+      <div className="player-config">
+        <Switch
+          checkedChildren="开启运行时自动滚动"
+          unCheckedChildren="禁用运行时自动滚动"
+          defaultChecked={autoScrollWhenPlay.current}
+          onChange={(e) => (autoScrollWhenPlay.current = e)}
+          style={{ marginBottom: 20 }}
+        />
+      </div>
+      <div className="player-panel" id="player-ground-1" ref={playerPanel}></div>
+      <TimelinePlayer timelineState={timelineState} autoScrollWhenPlay={autoScrollWhenPlay} />
+      <Timeline
+        style={{width:"100%", height: "100px"}}
+        scale={scale}
+        scaleWidth={scaleWidth}
+        startLeft={startLeft}
+        autoScroll={true}
+        ref={timelineState}
+        editorData={data}
+        effects={mockEffect}
+        onChange={(data) => {
+          setData(data as CusTomTimelineRow[]);
+          console.log("data changed: ", data);
+        }}
+        getActionRender={(action, row) => {
+          if (action.effectId === 'effect0') {
+            return <CustomRender0 action={action as CustomTimelineAction} row={row as CusTomTimelineRow} />;
+          } else if (action.effectId === 'effect1') {
+            return <CustomRender1 action={action as CustomTimelineAction} row={row as CusTomTimelineRow} />;
+          }
+        }}
+        autoReRender={true}
+      />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/mock.ts
@@ -0,0 +1,101 @@
+import { TimelineAction, TimelineEffect, TimelineRow } from '@xzdarcy/react-timeline-editor';
+import audioControl from './audioControl';
+import lottieControl from './lottieControl';
+
+export const scaleWidth = 160;
+export const scale = 5;
+export const startLeft = 20;
+
+export interface CustomTimelineAction extends TimelineAction {
+  data: {
+    src: string;
+    name: string;
+    subtitleNumber: number;
+    metaData: string;
+  };
+}
+
+export interface CusTomTimelineRow extends TimelineRow {
+  actions: CustomTimelineAction[];
+}
+
+export const mockEffect: Record<string, TimelineEffect> = {
+  effect0: {
+    id: 'effect0',
+    name: 'effect0',
+    source: {
+      start: ({ action, engine, isPlaying, time }) => {
+        if (isPlaying) {
+          const src = (action as CustomTimelineAction).data.src;
+          audioControl.start({ id: src, src, startTime: action.start, engine, time });
+          console.log("data: ", (action as CustomTimelineAction));
+        }
+      },
+      enter: ({ action, engine, isPlaying, time }) => {
+        if (isPlaying) {
+          const src = (action as CustomTimelineAction).data.src;
+          audioControl.start({ id: src, src, startTime: action.start, engine, time });
+        }
+      },
+      leave: ({ action, engine }) => {
+        const src = (action as CustomTimelineAction).data.src;
+        audioControl.stop({ id: src, engine });
+      },
+      stop: ({ action, engine }) => {
+        const src = (action as CustomTimelineAction).data.src;
+        audioControl.stop({ id: src, engine });
+      },
+    },
+  },
+  effect1: {
+    id: 'effect1',
+    name: 'effect1',
+    source: {
+      enter: ({ action, time }) => {
+        const src = (action as CustomTimelineAction).data.src;
+        lottieControl.enter({ id: src, src, startTime: action.start, endTime: action.end, time });
+        console.log("data: ", (action as CustomTimelineAction));
+      },
+      update: ({ action, time }) => {
+        const src = (action as CustomTimelineAction).data.src;
+        lottieControl.update({ id: src, src, startTime: action.start, endTime: action.end, time });
+      },
+      leave: ({ action, time }) => {
+        const src = (action as CustomTimelineAction).data.src;
+        lottieControl.leave({ id: src, startTime: action.start, endTime: action.end, time });
+      },
+    },
+  },
+};
+
+export const mockData: CusTomTimelineRow[] = [
+  {
+    id: '0',
+    actions: [
+      {
+        id: 'action3',
+        start: 0,
+        end: 20,
+        effectId: 'effect0',
+        data: {
+          src: '/lottie/lottie1/data.json',
+          name: 'Hello',
+          subtitleNumber: 1,
+          metaData: null,
+        },
+      },
+      {
+        id: 'action4',
+        start: 22,
+        end: 24,
+        effectId: 'effect1',
+        data: {
+          src: '/lottie/lottie1/data.json',
+          name: 'World',
+          subtitleNumber: 2,
+          metaData: null,
+        },
+      },
+    ],
+  },
+];
